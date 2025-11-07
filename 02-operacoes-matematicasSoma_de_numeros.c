07de22a4a24b9209833729a097f8541025380341
sasacatt <159670375+sasacatt@users.noreply.github.com>
2025-11-07 16:58:15 -0300

    Update and rename Soma_de_numeros.c to 02-operacoes-matematicasSoma_de_numeros.c

--- 02-operacoes-matematicasSoma_de_numeros.c
+++ 02-operacoes-matematicasSoma_de_numeros.c
@@ -7,18 +7,18 @@ int main(){
 	
 	setlocale (LC_ALL, "PORTUGUESE");
 
-printf("Digite o primeiro número inteiro:");
+printf("Digite o primeiro nÃºmero inteiro:");
 scanf("%d", &number1);
 
-printf("Digite o segundo número inteiro:");
+printf("Digite o segundo nÃºmero inteiro:");
 scanf("%d", &number2);
 
-printf("Digite o terceiro número inteiro:");
+printf("Digite o terceiro nÃºmero inteiro:");
 scanf("%d", &number3);
 
 produto= number1+number2+number3;
 
-printf("O valor da soma dos números é:%d\n", produto);
+printf("O valor da soma dos nÃºmeros Ã©:%d\n", produto);
 
 return 0;
 }
